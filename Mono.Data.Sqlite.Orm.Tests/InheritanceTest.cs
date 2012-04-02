9c846df6b7f83553f905ad82cd005d7487fe77c1
Matthew Leibowitz <mattleibowmail@gmail.com>
2012-04-02 13:48:03 +0200

    Added an Inheritance Unit Test

---
+++ Mono.Data.Sqlite.Orm.Tests/InheritanceTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Mono.Data.Sqlite.Orm.ComponentModel;
+using NUnit.Framework;
+
+namespace Mono.Data.Sqlite.Orm.Tests
+{
+    [TestFixture]
+    public class InheritanceTest
+    {
+        public class Base
+        {
+            [PrimaryKey]
+            public int Id { get; set; }
+
+            public string BaseProp { get; set; }
+        }
+
+        public class Derived : Base
+        {
+            public string DerivedProp { get; set; }
+        }
+
+        [Test]
+        public void InheritanceWorks()
+        {
+            var db = new OrmTestSession();
+
+            var mapping = db.GetMapping<Derived>();
+
+            Assert.AreEqual(3, mapping.Columns.Count);
+            Assert.AreEqual("Id", mapping.PrimaryKeys.First().Name);
+        }
+    }
+}
