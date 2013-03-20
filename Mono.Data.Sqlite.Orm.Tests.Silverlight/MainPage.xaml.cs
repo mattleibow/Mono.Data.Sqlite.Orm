749487fd2cfa4f1bf702c0e91e95ec07ba418eda
Matthew Leibowitz <mattleibow@live.com>
2013-03-20 19:57:16 +0200

    Used the wrong unit test framework for silverlight

--- Mono.Data.Sqlite.Orm.Tests.Silverlight/MainPage.xaml.cs
+++ Mono.Data.Sqlite.Orm.Tests.Silverlight/MainPage.xaml.cs
@@ -4,7 +4,7 @@
     using System.Windows;
     using System.Windows.Controls;
 
-    using Microsoft.Phone.Testing;
+    using Microsoft.Silverlight.Testing;
 
     public partial class MainPage : UserControl
     {
